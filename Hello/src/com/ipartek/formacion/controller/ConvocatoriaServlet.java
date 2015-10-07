485952c9ffe0bf1a25c98054ce0b032b595a3ffc
Administrador <Administrador@GBRB2.ipartekaula.com>
2015-10-07 16:07:13 +0200

    Convocatorias TODO

---
+++ Hello/src/com/ipartek/formacion/controller/ConvocatoriaServlet.java
@@ -0,0 +1,59 @@
+package com.ipartek.formacion.controller;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ConvocatoriaServlet
+ */
+public class ConvocatoriaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ConvocatoriaServlet() {
+		super();
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
---
+++ Hello/src/com/ipartek/formacion/service/ConvocatoriaService.java
@@ -0,0 +1,40 @@
+package com.ipartek.formacion.service;
+
+import java.util.List;
+
+import com.ipartek.formacion.bean.Convocatoria;
+import com.ipartek.formacion.service.interfaces.IConvocatoriaService;
+
+public class ConvocatoriaService implements IConvocatoriaService {
+
+	@Override
+	public List<Convocatoria> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int create(Convocatoria solicitud) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Convocatoria getById(int codigoSolicitud) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int update(Convocatoria solicitud) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean delete(int codigoSolicitud) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
